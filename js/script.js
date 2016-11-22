3e10eccb43c5a940620117f8b94edf9b9bd4ea19
aliya-niki <gardy444@gmail.com>
2016-11-22 13:10:07 +0300

    Исправила

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ var number1 = popup.querySelector("[name=adults-number]");
 var number2 = popup.querySelector("[name=children-number]");
 var form = popup.querySelector("form");
 
-window.onload=function(){
+window.onload=function() {
   popup.classList.add("modal-hidden");
 }
 
@@ -18,11 +18,32 @@ link.addEventListener("click", function(event) {
 
 form.addEventListener("submit", function(event) {
   event.preventDefault();
-
-  if ( !date1.value || !date2.value || !number1.value || !number2.value ) {
+  if (!date1.value || !date2.value || !number1.value || !number2.value) {
     console.log("Заполните все данные");
     popup.classList.remove("modal-error");
     popup.offsetWidth = popup.offsetWidth;
     popup.classList.add("modal-error");
   }
 });
+
+function initialize() {
+  var mapCoordinates = new google.maps.LatLng(34.86898673,-111.75292969);
+  var mapOptions = {
+    zoom: 9,
+    center: mapCoordinates,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+  };
+  var map = new google.maps.Map(document.querySelector(".map-canvas"), mapOptions);
+  var icon = new google.maps.MarkerImage ({
+    url: "img/ubicacion.png",
+    scaledSize: new google.maps.Size(50, 50),
+    origin: new google.maps.Point(0,0),
+    anchor: new google.maps.Point(0, 0)
+  });
+  var marker = new google.maps.Marker ({
+    position: mapCoordinates,
+    map: map,
+    icon: icon
+  });
+}
+google.maps.event.addDomListener(window, "load", initialize);
